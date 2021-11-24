a0cc6c63bd8dddbd4ad0066adb144d225b032bf5
Rustam <atabievrustam@mail.ru>
2021-11-24 21:35:45 +0300

    startProject

---
+++ src/js/script.js
@@ -0,0 +1,108 @@
+@@include('jquery-3.5.1.min.js')
+
+window.addEventListener('DOMContentLoaded', () => {
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//!Меню бургер
+// // // // // // // // // // // // // // // // // // // // //// // // // // // // // // // // // // // // // //
+	// const headerList = document.querySelector('.header__list');
+	// const headerItem = document.querySelectorAll('.header__item');
+	// const headerBurger = document.querySelector('.header__burger');
+
+	// headerBurger.addEventListener('click', () => {
+	//     headerBurger.classList.toggle('header__burger_active');
+	//     headerList.classList.toggle('header__list_active');
+	// });
+
+	// headerItem.forEach(item => {
+	//     item.addEventListener('click', () => {
+	//        headerBurger.classList.toggle('header__burger_active');
+	//         headerList.classList.toggle('header__list_active');
+	//     });
+	// }); 
+// // // // // // // // // // // // // // // // // // // // //// // // // // // // // // // // // // // // // //
+
+//!Слайдер
+// // // // // // // // // // // // // // // // // // // // //// // // // // // // // // // // // // // // // //
+	/*$('.slider').slick({
+		arrows: true,
+		dots: true,
+		adaptiveHeight: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		speed: 700,
+		easing: 'linear',
+		infinite: true,
+		initialSlide: 1,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		pauseOnFocus: true,
+		pauseOnHover: true,
+		pauseOnDotsHover: true,
+		draggable: true,
+		swipe: true,
+		touchThreshold: 5,
+		touchMove: true,
+		waitForAnimate: true,
+		centerMode: false,
+		variableWidth: false,
+		rows: 1,
+		slidesPerRow: 1,
+		vertical: false,
+		verticalSwiping: false,
+		fade: false,
+		// asNavFor: "класс слайдера с которым нужно связать";
+		responsive: [
+			{
+					breakpoint: 1000,
+					settings: {
+						slidesToShow: 2
+					}
+			},
+			{
+					breakpoint: 698,
+					settings: {
+						slidesToShow: 1
+					}
+			}
+		],
+		mobileFirst: false,
+	}); */
+// // // // // // // // // // // // // // // // // // // // //// // // // // // // // // // // // // // // // //
+	
+//!Код к спойлерам
+// // // // // // // // // // // // // // // // // // // // //// // // // // // // // // // // // // // // // //
+/* $('.spoilers__title').click(function () {
+		if ($('.spoilers').hasClass('one')) {
+			$('.spoilers__title').not($(this)).removeClass('active');
+			$('.spoilers__text').not($(this).next()).slideUp(300);
+		}
+		$(this).toggleClass('active').next().slideToggle(300);
+	});*/
+// // // // // // // // // // // // // // // // // // // // //// // // // // // // // // // // // // // // // //
+});
